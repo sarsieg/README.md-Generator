256fb01df79bb41346ecec5a32a075e627325e84
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-06-15 23:07:54 -0500

    added function to generate the readme.md file with the answers answered in the prompts, then to console.log a message stating they have successfully created the file

--- index.js
+++ index.js
@@ -37,51 +37,56 @@ Email: ${answers.email}`
 
 // starting prompts for the questions in the terminal
 inquirer.prompt([{
-    type: 'input',
-    name: 'title',
-    message: 'What is the title of the project?',
-},
-{
-    type: 'input',
-    name: 'description',
-    message: 'Description of the project.',
-},
-{
-    type: 'input',
-    name: 'install',
-    message: 'What are the installation instructions?',
-},
-{
-    type: 'input',
-    name: 'usage',
-    message: 'What are the usage instructions?',
-},
-{
-    type: 'checkbox',
-    name: 'license',
-    message: 'What license would you like?',
-    choices: ['MIT', 'GNU', 'Apache'],
-},
-{
-    type: 'input',
-    name: 'contribution',
-    message: 'What are the instructions for contributing?',
-},
-{
-    type: 'input',
-    name: 'test',
-    message: 'Do you have any tests to add?',
-},
-{
-    type: 'input',
-    name: 'gitHub',
-    message: 'What is your gitHub username?',
-},
-{
-    type: 'input',
-    name: 'email',
-    message: 'What is an email people can send questions to?'
-}
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of the project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Description of the project.',
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'What are the installation instructions?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What are the usage instructions?',
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'What license would you like?',
+            choices: ['MIT', 'GNU', 'Apache'],
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'What are the instructions for contributing?',
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Do you have any tests to add?',
+        },
+        {
+            type: 'input',
+            name: 'gitHub',
+            message: 'What is your gitHub username?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is an email people can send questions to?'
+        }
 
-
-}])
+    ])
+    .then((answers) => {
+        const READMEcontent = generateREADME(answers);
+        fs.writeFile('README.md', READMEcontent, (err) =>
+            err ? console.log(err) : console.log('Successfully created README.md')
+        );
+    });
