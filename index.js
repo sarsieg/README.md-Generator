12c200f4fcf7bc27828e818dd950ae4fed95f0ad
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-06-15 22:45:50 -0500

    added description category and table of contents with description, intallation and usage subcategories

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-// installing inquire and license and starting the string literal
+// installing inquire4 and license and starting the string literal
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateREADME = (answers) =>
     `## ${answers.title} [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+## Description
+${answers.description}.
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
